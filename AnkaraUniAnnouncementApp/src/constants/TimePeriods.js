e0f2c76074ac47e8b8726d10de1838438b1f5745
Altan SOZUER <sozueraltan@gmail.com>
2023-05-05 20:23:20 +0300

    TimePeriods and WebsitesOfDepartments constants are added for filtering

---
+++ AnkaraUniAnnouncementApp/src/constants/TimePeriods.js
@@ -0,0 +1,7 @@
+const TIME_PERIODS = [
+    "Last 24 Hours",
+    "Last 1 Week",
+    "Last 1 Month"
+]
+
+export default TIME_PERIODS;
---
+++ AnkaraUniAnnouncementApp/src/constants/WebsitesOfDepartments.js
@@ -0,0 +1,9 @@
+const WEBSITES = [
+    "Ankara Üniversitesi Website",
+    "Ankara Üniversitesi Bilgisayar Mühendisliği",
+    "Ankara Üniversitesi SKS"
+]
+
+//TODO: Add other website addresses of departments
+
+export default WEBSITES;
